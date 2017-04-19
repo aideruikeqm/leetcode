da04b1f3c2e6006d3ef69289bed808db06674aa2
aideruikeqm <mzxjob@hotmail.com>
2017-04-19 15:31:59 +0100

    Update En_DecodeTinyURL.py

--- En_DecodeTinyURL.py
+++ En_DecodeTinyURL.py
@@ -1,3 +1,11 @@
+'''
+Question:
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and
+it returns a short URL such as http://tinyurl.com/4e9iAk.
+Design the encode and decode methods for the TinyURL service. 
+There is no restriction on how your encode/decode algorithm should work. 
+You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+'''
 class Codec:
 
     alphabet = string.ascii_letters + '0123456789'
