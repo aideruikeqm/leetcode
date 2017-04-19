1d25ca81c6848bc8b13e7cfcbae2eedc343b48cd
aideruikeqm <mzxjob@hotmail.com>
2017-04-19 15:19:33 +0100

    Update Hamming_Distance.py

--- Hamming_Distance.py
+++ Hamming_Distance.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
+        #one line code:
+        return bin(x^y).count('1')
+        
+        #another solution:
+        '''
+        res = 0
+        for c in str(bin(x ^ y)):
+            if c == '1':
+                res += 1
+
+        return res
+        '''
