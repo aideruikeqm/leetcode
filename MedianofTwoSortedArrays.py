49d275d1e54fbc7b828bcc3dc8b2acf451f1e347
aideruikeqm <mzxjob@hotmail.com>
2017-04-28 16:06:03 +0100

    Create MedianofTwoSortedArrays.py

---
+++ MedianofTwoSortedArrays.py
@@ -0,0 +1,43 @@
+'''
+Question:
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+The median is (2 + 3)/2 = 2.5
+'''
+
+  class Solution:
+    """
+    @param A: An integer array.
+    @param B: An integer array.
+    @return: a double whose format is *.5 or *.0
+    """
+    def findMedianSortedArrays(self, A, B):
+        n = len(A) + len(B)
+        if n % 2 == 1:
+            return self.findKth(A, B, n / 2 + 1)
+        else:
+            smaller = self.findKth(A, B, n / 2)
+            bigger = self.findKth(A, B, n / 2 + 1)
+            return (smaller + bigger) / 2.0
+
+    def findKth(self, A, B, k):
+        if len(A) == 0:
+            return B[k - 1]
+        if len(B) == 0:
+            return A[k - 1]
+        if k == 1:
+            return min(A[0], B[0])
+        
+        a = A[k / 2 - 1] if len(A) >= k / 2 else None
+        b = B[k / 2 - 1] if len(B) >= k / 2 else None
+        
+        if b is None or (a is not None and a < b):
+            return self.findKth(A[k / 2:], B, k - k / 2)
+        return self.findKth(A, B[k / 2:], k - k / 2)
